ca9d18a12e4c52294d2eda59ed970720fa94d438
Abhimanyu Kolakotla19 <abhimanyu.kolakotla@in.ibm.com>
2022-02-21 21:21:59 +0530

    Fixed layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,41 @@ class XylophoneApp extends StatelessWidget {
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              buildButton(Colors.red, "note1.wav"),
-              buildButton(Colors.orange, "note2.wav"),
-              buildButton(Colors.yellow, "note3.wav"),
-              buildButton(Colors.green, "note4.wav"),
-              buildButton(Colors.green.shade900, "note5.wav"),
-              buildButton(Colors.blue, "note6.wav"),
-              buildButton(Colors.purple, "note7.wav"),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Xylophone",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    buildButton(Colors.red, "note1.wav"),
+                    buildButton(Colors.orange, "note2.wav"),
+                    buildButton(Colors.yellow, "note3.wav"),
+                    buildButton(Colors.green, "note4.wav"),
+                    buildButton(Colors.green.shade900, "note5.wav"),
+                    buildButton(Colors.blue, "note6.wav"),
+                    buildButton(Colors.purple, "note7.wav"),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
