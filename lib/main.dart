6c7cd28a77492ce3c536c981c6ed79e115a1d1a7
Abhimanyu Kolakotla19 <abhimanyu.kolakotla@in.ibm.com>
2022-02-21 19:47:06 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,44 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  runApp(XylophoneApp());
+}
 
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildButton(Colors.red, "note1.wav"),
+              buildButton(Colors.orange, "note2.wav"),
+              buildButton(Colors.yellow, "note3.wav"),
+              buildButton(Colors.green, "note4.wav"),
+              buildButton(Colors.green.shade900, "note5.wav"),
+              buildButton(Colors.blue, "note6.wav"),
+              buildButton(Colors.purple, "note7.wav"),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget buildButton(Color color, String note) {
+    return Expanded(
+      child: TextButton(
+        style:
+            TextButton.styleFrom(backgroundColor: color, primary: Colors.white),
+        onPressed: () {
+          var audioPlayer = AudioCache();
+          audioPlayer.play(note);
+        },
       ),
     );
   }
